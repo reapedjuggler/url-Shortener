36f39aee84668fe1149765ed51e6e36db889787f
reapedjuggler <tomarvibhav55@gmail.com>
2023-05-20 16:51:46 +0530

    Added the redis config, the utility function and the initial setup for the project

---
+++ controllers/resolveController.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"reapedjuggler/url-shortener/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+)
+
+func Resolve(ctx *gin.Context) { // resolving a url
+	// ctx.JSON(http.StatusAccepted, "fuck world")
+
+	// code, _ := ctx.Params.Get("shorturl")
+
+	code := ctx.Request.URL.Query().Get("shorturl")
+
+	// write the logic to find the corresponding original
+	// url and reirect it
+
+	var client *redis.Client = utils.GetClient()
+
+	// fmt.Println("code\n", code, " -> \n sad", code1)
+
+	val, err := client.Get(code).Result()
+
+	if err != nil {
+		fmt.Println(err)
+		ctx.JSON(http.StatusNotFound, "The given short url is invalid")
+	}
+
+	fmt.Println(val, "\nval")
+
+	loginUrl := val // "httsp://www.google.com/"
+
+	// rdirect it here
+
+	ctx.Redirect(http.StatusFound, loginUrl)
+
+}
---
+++ utils/config.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"github.com/go-redis/redis"
+)
+
+var client *redis.Client = nil
+
+func InitRedis() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+}
+
+func GetClient() *redis.Client {
+
+	if client == nil {
+		InitRedis()
+	}
+
+	return client
+}
