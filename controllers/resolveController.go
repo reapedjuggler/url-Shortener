89510e12f02c2e0dd796b4452d91ee5eb709c84f
reapedjuggler <tomarvibhav55@gmail.com>
2023-08-28 01:33:26 +0530

    Working great, next identify how much load it can take :rocket:

--- controllers/resolveController.go
+++ controllers/resolveController.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"reapedjuggler/url-shortener/utils"
@@ -33,32 +31,23 @@ func Resolve(ctx *gin.Context) {
 
 	// Cache lookup
 	if err == nil {
-		// fmt.Println(err, " err")
-		// ctx.JSON(http.StatusNotFound, "The given short url is invalid")
 		log.Print(val, " Corresponding Resolved URL")
-		fmt.Println(val, "\nval")
-		ctx.Redirect(http.StatusMovedPermanently, val)
 		return
 	}
+
+	log.Print("Cache miss occurred for shorturl = ", code)
 
 	// Cache miss, lookup in mongodb
 	mongoClient := utils.GetMongoClient()
 	db := mongoClient.Database(utils.GetKeyFromEnv(utils.DatabaseName))
 	coll := db.Collection(utils.GetKeyFromEnv(utils.CollectionName))
 	filter := bson.D{{Key: "urls", Value: code}}
-	cursor, err := coll.Find(context.TODO(), filter)
-	if err != nil {
+	var correspondingUrl ResultFromMongoDB = ResultFromMongoDB{}
+	errFromFindOne := coll.FindOne(context.TODO(), filter).Decode(&correspondingUrl)
+	if errFromFindOne != nil {
+		ctx.JSON(http.StatusNotFound, "The given short url is invalid")
 		panic(err)
 	}
-	log.Print(code, " . ", cursor)
-	results := []ResultFromMongoDB{}
-	if err = cursor.All(context.TODO(), &results); err != nil {
-		panic(err)
-	}
-
-	for _, result := range results {
-		res, _ := json.Marshal(result)
-		// log.Print(string(res), " res")
-		fmt.Println(string(res))
-	}
+	log.Print(correspondingUrl, " correspondingUrl")
+	ctx.Redirect(http.StatusMovedPermanently, correspondingUrl.Longurl)
 }
